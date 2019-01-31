eacd403f3d38327e784e269080f7fada105c2de2
chenj5618 <76765168@qq.com>
2019-01-31 08:43:14 +0800

    初始化

---
+++ basetype/src/main/java/cn/zb/basetype/App.java
@@ -0,0 +1,13 @@
+package cn.zb.basetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
