dd32b75e7fb429e1a6fc81d51ab835516250c042
chenj5618 <76765168@qq.com>
2019-01-31 08:54:39 +0800

    基本类型

---
+++ basetype/src/main/java/cn/zb/basetype/BaseType.java
@@ -0,0 +1,33 @@
+package cn.zb.basetype;
+
+/**
+ * java 8种基本类型
+ * 
+ * @author chen
+ *
+ */
+public class BaseType {
+
+	public static void main(String[] args) {
+
+		int a = 0;
+		long b = 0L;
+		short c = 0;
+		char d = '0';
+		float e = 0.0F;
+		double f = 0.0;
+
+		boolean g = false;
+
+		byte h = 0;
+		// 1、8种基本类型的取值范围
+		a=Integer.MAX_VALUE;
+		System.out.println("int 类型的最大值:"+a);
+		a=Integer.MIN_VALUE;
+		System.out.println("int 类型的最小值:"+a);
+		
+		
+
+	}
+
+}
